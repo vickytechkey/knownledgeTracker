9361ed773b93d804bd336be88cce1b7480bdc099
vickytechkey <vichunice@gmail.com>
2026-04-26 09:50:09 +0530

    fixing positive integer

--- fields/positive_integer.py
+++ fields/positive_integer.py
@@ -1,11 +1,14 @@
-class positiveInteger:
-    def __set_name__(self, owner, value):
-        self.positive_integer = '_' + value
+class PositiveInteger:
+    def __set_name__(self, owner, name):
+        self.private_name = "_" + name
 
     def __get__(self, instance, owner):
-        return getattr(instance, self.positive_integer)
+        if instance is None:
+            return self
+        return getattr(instance, self.private_name, None)
 
     def __set__(self, instance, value):
         if not isinstance(value, int) or value <= 0:
             raise ValueError("Value must be a positive integer")
-        setattr(instance, self.positive_integer, value)
+        setattr(instance, self.private_name, value)
+        
